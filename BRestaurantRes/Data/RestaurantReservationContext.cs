fee402cb87c466a7fcab7aa59bd1d2e008292cfb
csgun <csgun@LAPTOP-LR3RK1IO>
2021-11-16 14:22:16 +0100

    ss

---
+++ BRestaurantRes/Data/RestaurantReservationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BRestaurantReservation.Data
+{
+    public class RestaurantReservationContext : DbContext
+    {
+        public RestaurantReservationContext (DbContextOptions<RestaurantReservationContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DRestaurantReservation.MAvaialbleTables> MAvaialbleTables { get; set; }
+        public DbSet<DRestaurantReservation.TTableReservation> TTableReservation { get; set; }
+    }
+}
--- ResetaurantRes/Startup.cs
+++ ResetaurantRes/Startup.cs
@@ -1,20 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using BRestaurantRes.Data;
+using BRestaurantReservation.Data;
 
-namespace ResetaurantRes
+namespace RestaurantReservation
 {
     public class Startup
     {
@@ -22,7 +15,6 @@ namespace ResetaurantRes
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -35,8 +27,8 @@ namespace ResetaurantRes
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ResetaurantRes", Version = "v1" });
             });
 
-            services.AddDbContext<ResetaurantResContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ResetaurantResContext")));
+            services.AddDbContext<RestaurantReservationContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RestaurantReservationContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
